eb66a7b4e995140aae7d727ca36b44487f3d9119
misskteague <kteague@greycourt.org.uk>
2018-08-10 10:05:08 +0000

    Update TeacherDevice_Cycle1.py
    Starting to test the serial connection validation
    Error message:
    
    Traceback (most recent call last):
      File "/usr/lib/python3/dist-packages/serial/serialposix.py", line 490, in read
        'device reports readiness to read but returned no data '
    serial.serialutil.SerialException: device reports readiness to read but returned no data (device disconnected or multiple access on port?)
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/home/pi/Desktop/ConvertToPixels v4.py", line 73, in <module>
        studentStates = radioRecieve(sense,studentStates,classroom)
      File "/home/pi/Desktop/ConvertToPixels v4.py", line 21, in radioRecieve
        data = s.readline().decode('UTF-8') #UTF-8 is a method of unicode endcoding that microbits use for serial connections
      File "/usr/lib/python3/dist-packages/serial/serialposix.py", line 497, in read
        raise SerialException('read failed: {}'.format(e))
    serial.serialutil.SerialException: read failed: device reports readiness to read but returned no data (device disconnected or multiple access on port?)
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/usr/lib/python3/dist-packages/serial/serialposix.py", line 265, in open
        self.fd = os.open(self.portstr, os.O_RDWR | os.O_NOCTTY | os.O_NONBLOCK)
    OSError: [Errno 19] No such device: '/dev/ttyACM2'
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/home/pi/Desktop/ConvertToPixels v4.py", line 75, in <module>
        s = serial.Serial("/dev/ttyACM2").close() #closes the serial connection
      File "/usr/lib/python3/dist-packages/serial/serialutil.py", line 236, in __init__
        self.open()
      File "/usr/lib/python3/dist-packages/serial/serialposix.py", line 268, in open
        raise SerialException(msg.errno, "could not open port {}: {}".format(self._port, msg))
    serial.serialutil.SerialException: [Errno 19] could not open port /dev/ttyACM2: [Errno 19] No such device: '/dev/ttyACM2'
    >>>

--- TeacherDevice_Cycle1.py
+++ TeacherDevice_Cycle1.py
@@ -1,20 +1,58 @@
 import serial
 from sense_hat import SenseHat
 
+def setClassroom(classroom,sense):
+  sense.clear() #clears the screen of any coloured pixels
+  for key in classroom:
+    sense.set_pixel(classroom[key][0],classroom[key][1],(0, 0, 255))
+
+def radioRecieve(sense,studentStates,classroom):
+  
+  #setup of the serial connections
+  PORT = "/dev/ttyACM2" #USB port that the reciever Micro:bit is connected to - ls /dev/ttyA*
+  BAUD = 115200 #bits per second
+  s = serial.Serial(PORT)
+  s.baudrate = BAUD
+  s.parity   = serial.PARITY_NONE
+  s.databits = serial.EIGHTBITS
+  s.stopbits = serial.STOPBITS_ONE
+
+  #Data being readin from the serial connection
+  data = s.readline().decode('UTF-8') #UTF-8 is a method of unicode endcoding that microbits use for serial connections
+  data = data.rstrip() #removes any bits that aren't the data
+  print(data) #not essential - used for testing to output to shell the recieved data
+  
+  studentUpdate = ""
+  studentUpdate = data.split(",") #data is packaged with a , to differenitate between the ID and state - splits to a list
+ 
+  #validation against invalid recieved messages
+  try:
+      int(studentUpdate[0]) #validation to check the ID is a number
+      if studentUpdate[1] not in ["R","A","Y","G"]: #validation to check state is in valid range
+          print("Invalid state recieved")
+      return changeState(sense,studentUpdate,studentStates,classroom)
+  except ValueError:
+      print("Invalid ID recieved") #if the ID cannot be changed to an integer this message shows on console
+  except IndexError:
+      print("Invalid state recieved") #if the studentUpdate doesn't have an index 1 this error will show on the console
+      
+
+def changeState(sense,studentUpdate,studentStates,classroom):
+  
+  #colours dict {"key": (R,G,B)}
+  colours = {"R":(255,0,0),"A":(255,165,0),"Y":(255,255,0),"G":(0,255,0)}
+
+ 
+  #set_pixel(x,y,(r,g,b))
+  sense.set_pixel(classroom[studentUpdate[0]][0],classroom[studentUpdate[0]][1],colours[studentUpdate[1]]) #updates the pixel
+  studentStates[int(studentUpdate[0])-1] = studentUpdate[1] #updates the states list with the new student state
+  print(studentStates) #not essential - used for testing to show the updated list of studentStates
+  return studentStates #this is inplace of using studentState as a global variable
+  
+  
+  
 sense = SenseHat()
 
-PORT = "/dev/ttyACM1" #may need to change this.  ls /dev/ttyA*
-BAUD = 115200 #bits per second
-
-s = serial.Serial(PORT)
-s.baudrate = BAUD
-s.parity   = serial.PARITY_NONE
-s.databits = serial.EIGHTBITS
-s.stopbits = serial.STOPBITS_ONE
-
-studentUpdate = ""
-
-colours = {"R":(255,0,0),"A":(255,165,0),"Y":(255,255,0),"G":(0,255,0)}
 
 #classroom dict {"key":(x,y)}
 classroom = {"1":(0,1),"2":(0,2),"3":(0,3),"4":(0,4),"5":(0,5),"6":(0,6),"7":(0,7),
@@ -22,27 +60,17 @@ classroom = {"1":(0,1),"2":(0,2),"3":(0,3),"4":(0,4),"5":(0,5),"6":(0,6),"7":(0,
              "16":(4,0),"17":(4,1),"18":(4,2),"19":(4,3),"20":(4,4),"21":(4,5),"22":(4,6),"23":(4,7),
              "24":(7,7),"25":(7,6),"26":(7,5),"27":(7,4),"28":(7,3),"29":(7,2),"30":(7,1),"31":(7,0)}
 
+#Uses the ID read in from the serial connection on the attached microbit
 studentStates = ["G","G","G","G","G","G","G",
                  "G","G","G","G","G","G","G","G",
                  "G","G","G","G","G","G","G","G",
                  "G","G","G","G","G","G","G","G"]
-sense.clear()
+
+setClassroom(classroom,sense)
 while True:
-    data = s.readline().decode('UTF-8')
-    data = data.rstrip()
-    print(data)
-    studentUpdate = data.split(",")
-    print(studentUpdate)
-    try:
-        int(studentUpdate[0])
-        if studentUpdate[1] not in ["R","A","Y","G"]:
-            print("Invalid state recieved")
-        sense.set_pixel(classroom[studentUpdate[0]][0],classroom[studentUpdate[0]][1],colours[studentUpdate[1]])
-        studentStates[int(studentUpdate[0])-1] = studentUpdate[1]
-        print(studentStates)
-    except ValueError:
-        print("Invalid ID recieved")
-    except IndexError:
-        print("Invalid state recieved")
+  #validation against the serial connection being removed
+  try:
+    studentStates = radioRecieve(sense,studentStates,classroom)
+  except serial.serialutil.SerialException:
+    s = serial.Serial("/dev/ttyACM2").close() #closes the serial connection
     
-s.close()
