4a5c2e0145a1559f22b3e6f459359f0ef5594813
misskteague <kteague@greycourt.org.uk>
2018-10-22 17:04:06 +0100

    Update GUI Class - Cycle 3.py

--- GUI Class - Cycle 3.py
+++ GUI Class - Cycle 3.py
@@ -1,8 +1,27 @@
-from Tkinter import * 
+from tkinter import *
 Class GUI:
 
-  def __init__(dimensions):
+  def __init__(self,height, width, title):
     #set clour
     #set dimensions
     #set resizable
+    self.root = Tk()
+    self.root.title("{}").format(title)
+    self.root.geometry("{}x{}").format(height,width)
+    self.root.resizable (False, False)
+    self.root.configure(background = "white")
+
+Class Select(GUI,Database): #need to import from database to return database
+  
+  def __init__(self,height, width, title):
+    super().__init__(self,height, width, title)
+    
+    
+  #need to return the list of names  
+  for i,url in enumerate(url_list):
+    label=tk.Label(frame,text=url)
+    label.grid(row=i)
+    label.bind("<Button-1>",lambda e,url=url:open_url(url))
+  
+    
     
