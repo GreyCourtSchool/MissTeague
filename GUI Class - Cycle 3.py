9acdf92413e4da0769f05b95e18b8bf52b01c291
misskteague <kteague@greycourt.org.uk>
2018-10-24 19:37:55 +0000

    Update GUI Class - Cycle 3.py
    Tested the init method for the super class and started planning the next round of testing with Select

--- GUI Class - Cycle 3.py
+++ GUI Class - Cycle 3.py
@@ -1,27 +1,41 @@
 from tkinter import *
-Class GUI:
+from DatabaseClass import Database
+class GUI:
 
-  def __init__(self,height, width, title):
+   def __init__(self,width, height, title):
     #set clour
     #set dimensions
     #set resizable
     self.root = Tk()
-    self.root.title("{}").format(title)
-    self.root.geometry("{}x{}").format(height,width)
+    self.root.title(title)
+    #self.root.title("{}").format(title)
+    dimension = str(width)+"x"+str(height)
+    self.root.geometry(dimension)
+    #self.root.geometry("{}x{}").format(height,width)
     self.root.resizable (False, False)
     self.root.configure(background = "white")
 
-Class Select(GUI,Database): #need to import from database to return database
+    #self.root.mainloop() we need this not sure where!
+  def runWindow(self):
+    self.root.mainloop()
+
+  def openFile(filename):
+    file = open(filename+".text",r)
+    #needs to open the file
+    #pull out the data
+    #return list of contents
+
+class Select(GUI): #need to import from database to return database
   
   def __init__(self,height, width, title):
     super().__init__(self,height, width, title)
     
-    
-  #need to return the list of names  
-  for i,url in enumerate(url_list):
-    label=tk.Label(frame,text=url)
+  #result = Database.returnSQL("SELECT className FROM classesTable") 
+  #need to return the list of names
+  result = super().openFile()
+  for i,result in enumerate(result):
+    label=self.root.Label(frame,text=result)
     label.grid(row=i)
-    label.bind("<Button-1>",lambda e,url=url:open_url(url))
-  
-    
-    
+    #label.bind("<Button-1>",lambda e,url=url:open_url(url))
+
+
