1a4b7801a527854c850f065a4ec2f08d207473c8
misskteague <kteague@greycourt.org.uk>
2018-10-26 18:28:26 +0000

    Update GUI Class - Cycle 3.py
    updated based on gui zero rather than tkinter. Need to work out how to start the app n the first instance then change to windows for the rest of the project.

--- GUI Class - Cycle 3.py
+++ GUI Class - Cycle 3.py
@@ -1,38 +1,32 @@
-from tkinter import *
+from guizero import App, PushButton, Window, Text, TextBox
 from DatabaseClassv2 import Database
+'''
 class GUI:
-
-   def __init__(self,width, height, title, root):
-    #set clour
-    #set dimensions
-    #set resizable
-    #self.root = Tk()
-    self.root = root
-    root.title(title)
-    #self.frame = Frame(self.root)
-    #self.root.title("{}").format(title)
-    dimension = str(width)+"x"+str(height)
-    self.root.geometry(dimension)
-    #self.root.geometry("{}x{}").format(height,width)
-    self.root.resizable (False, False)
-    self.root.configure(background = "white")
-
-    #self.root.mainloop() we need this not sure where!
-   def runWindow(self):
-    self.root.mainloop()
-
+   def __init__(self,width, height, title):
+      self.app = App(title=title,width=width,height=height,layout="grid",bg="white")
+      #self.app = App()
+      #self.app.display()
+      #self.window = Window(app,title=title,width=width,height=height,layout="grid",bg="white")
+'''
+class NewWindow():
+   
+   def __init__(self,width, height, title):
+      if not self.app:
+         self.app = App(title=title,width=width,height=height,layout="grid",bg="white")
+      self.window = Window(self.app,title=title,width=width,height=height,layout="grid",bg="white")
+      self.window.show()
+      
    def openFile(self,filename):
-    self.contents = []
-    with open(filename+".txt","r") as self.file:
-       for line in self.file:
-          self.contents.append(line.split(","))
-       return self.contents
+      self.contents = []
+      with open(filename+".txt","r") as self.file:
+         for line in self.file:
+            self.contents.append(line.split(","))
+      return self.contents
 
-
-class Select(GUI,Database): #need to import from database to return database
+class Select(NewWindow,Database): #need to import from database to return database
   
-  def __init__(self,height, width, title, root):
-    super().__init__(height, width, title, root)
+  def __init__(self,height, width, title):
+    super().__init__(height, width, title)
     
   #result = Database.returnSQL("SELECT className FROM classesTable") 
   #need to return the list of names
@@ -41,21 +35,25 @@ class Select(GUI,Database): #need to import from database to return database
     result = super().openFile("users")
     print(result)
     for entry in range(len(result)):
-       self.button=Button(self.root,text=result[entry][0],command=lambda:self.openUser(result[entry][1]))
-       self.button.grid(row=entry)
-    self.button = Button(self.root,text="New User...",command=self.newUser).grid(row=len(result)
+       self.button=PushButton(self.app ,text=result[entry][0],command=lambda:self.openUser(result[entry][1]),grid=[0,entry])
+       #self.button.grid(row=entry)
+    self.button = PushButton(self.app,text="New User...",command=self.newUser,grid=[0,len(result)])
+
+  def test(self):
+     print("test")
 
   def newUser(self):
      print("new user")
-     self.root.destroy()
-     super().__init__(250, 100, "New User", Tk())
-     self.label=Label(self.root,text="Enter the your name\nE.g. Mr Hirst").grid(row=0)
-     self.entry = Entry(self.root,width=10)
-     self.entry.grid(row=1)
-     self.button = Button(self.root,text="Submit",command=self.submitNewUser).grid(row=2)
+     self.window.hide()
+     super().__init__(250, 100, "New User")
+     self.text=Text(self.app,text="Enter the your name\nE.g. Mr Hirst",grid=[0,0])
+     self.entry = TextBox(self.app,width=10)
+     
+     self.button = PushButton(self.app,text="Submit",command=self.submitNewUser,grid=[0,1])
 
   def submitNewUser(self):
      self.user = self.entry.get()
      self.user = self.user.replace(" ","")
      self.openUser(self.user)
      print("new database created")
+     
